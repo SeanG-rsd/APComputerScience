6f8303fe9d94fe158f81983a27eb60fb861a45dc
SeanG-rsd <seagut20@gmail.com>
2023-09-11 08:32:29 -0700

    Intro Output

---
+++ CSAssignment-Output/src/Hello.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Hello {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+        System.out.println();
+        System.out.println();
+        System.out.println("Life is roblox");
+        System.out.println("Call me asparagus");
+
+        System.out.println();
+        drawBox();
+        drawBox();
+    }
+
+    public static void drawBox() {
+        System.out.println("+-----+");
+        System.out.println("|     |");
+        System.out.println("|     |");
+        System.out.println("+-----+");
+    }
+}
