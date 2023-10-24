4429612796b790851df50435fe2b64d7f4c0dab3
SeanG-rsd <70712778+SeanG-rsd@users.noreply.github.com>
2023-10-23 17:46:25 -0700

    gs12 selection sort

--- GoalSheet12/src/GSTwelveProjects.java
+++ GoalSheet12/src/GSTwelveProjects.java
@@ -7,7 +7,10 @@ public class GSTwelveProjects
     {
         //binarySearch();
 
-        pointComparator();
+        //pointComparator();
+        int[] array = new int[]{6,3,0,1,4,2,5,7,10,3,45,7};
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
     public static void binarySearch() throws FileNotFoundException
@@ -56,5 +59,47 @@ public class GSTwelveProjects
         List<Point> points = new LinkedList<>();
         points.add(new Point(3, 3));
         points.add(new Point(2, 2));
+
+        points.sort(new PointComparator());
+    }
+
+    public static void selectionSort(int[] input)
+    {
+        for (int i = input.length - 1; i >= 0; --i)
+        {
+            int biggest = i;
+            int smallest = input.length - 1 - i;
+
+            for (int j = i - 1; j >= 0; --j)
+            {
+                if (input[j] > input[biggest])
+                {
+                    biggest = j;
+                }
+                if (input[input.length - 1 - j] < input[smallest])
+                {
+                    smallest = i - j;
+                }
+            }
+
+            swap(input, i, biggest);
+            swap(input, input.length - 1 - i, smallest);
+            System.out.println(i + "  " + (input.length - 1 - i));
+            System.out.println(biggest + "  " + smallest);
+            System.out.println(Arrays.toString(input));
+        }
+    }
+
+    public static void swap(int[] input, int i, int j)
+    {
+        int temp = input[j];
+        input[j] = input[i];
+        input[i] = temp;
+
+    }
+
+    public static void dataProcessing()
+    {
+
     }
 }
